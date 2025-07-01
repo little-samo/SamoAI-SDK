5b05aa62f497e832eef99c3a224ba6c4def71119
Lucid <lucid@littlesamo.io>
2025-07-01 16:51:25 +0900

    Simplify dto by removing unused fields

--- src/dto/entities/entity.ts
+++ src/dto/entities/entity.ts
@@ -2,8 +2,5 @@ import { EntityId } from '@little-samo/samo-ai';
 
 export interface EntityDto {
   id: EntityId;
-  handle?: string;
   name: string;
-  appearance: string;
-  expression?: string;
 }
