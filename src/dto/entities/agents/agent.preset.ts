f1f2b57dfdbcc540262dc57ead14a2535ac213cd
Lucid <lucid@littlesamo.io>
2025-07-06 12:31:29 +0900

    Add AgentPresetDto.ownerUser

--- src/dto/entities/agents/agent.preset.ts
+++ src/dto/entities/agents/agent.preset.ts
@@ -1,11 +1,14 @@
 import { AgentConfig } from '@little-samo/samo-ai-sdk/models';
 
+import { UserPublicDto } from '../users';
+
 export interface AgentPresetDto {
   id: bigint;
   presetName: string;
   presetShortDescription: string;
   presetDescription: string;
   config: Partial<AgentConfig>;
+  ownerUser?: UserPublicDto;
   createdAt: Date;
   updatedAt: Date;
 }
