8f668758cfe3cb0a10bbab945f6eebfc302da420
Lucid <lucid@littlesamo.io>
2025-09-01 17:29:50 +0900

    Add param1 and param2 to ItemDto interfaces

--- src/dto/items/item.ts
+++ src/dto/items/item.ts
@@ -4,10 +4,14 @@ export interface ItemDto {
   name: string;
   description?: string;
   count: number;
+  param1: number;
+  param2: number;
   updatedAt: Date;
 }
 
 export interface ItemUpdateDto {
   count: number;
+  param1: number;
+  param2: number;
   updatedAt: Date;
 }
