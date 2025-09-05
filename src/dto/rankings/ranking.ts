b78a794a4bea8e1b4f5ef71a8c0f438d1335487c
Lucid <lucid@littlesamo.io>
2025-09-05 14:02:18 +0900

    Add rank field to RankingDto

--- src/dto/rankings/ranking.ts
+++ src/dto/rankings/ranking.ts
@@ -3,6 +3,7 @@ import { AgentId, LocationId, UserId } from '@little-samo/samo-ai';
 export interface RankingDto {
   type: number;
   date: Date;
+  rank: number;
   locationId?: LocationId;
   agentId?: AgentId;
   userId?: UserId;
